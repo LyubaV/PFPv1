fde4f1b796af9a02a0f2a0e8bc00211b6dd8e674
Capt Smollet <capt.smollet@hotmail.com>
2013-02-09 16:05:57 +0400

    initial

--- weblayer/src/main/java/com/sitronics/learning/pfp/weblayer/TestController.java
+++ weblayer/src/main/java/com/sitronics/learning/pfp/weblayer/TestController.java
@@ -1,21 +1,51 @@
 package com.sitronics.learning.pfp.weblayer;
 
+import com.fasterxml.jackson.core.JsonFactory;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.sitronics.learning.pfp.persist.AccountsBean;
+import org.omg.PortableInterceptor.ObjectReferenceTemplateSeqHelper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.support.RequestContext;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.logging.Logger;
 
 @Controller
 public class TestController {
 
     @Autowired
     ApplicationContext context;
+    @Autowired
+    AccountsBean accountsBean;
+
+    Logger logger = Logger.getLogger(this.getClass().toString());
 
     @RequestMapping("/pfp")
     public String testRequest(Model model) {
+        //logger.info("Test");
         model.addAttribute("acc", context.getBean("accountsBean"));
         return "testpage";
+    }
+
+    @RequestMapping(value = "/service")
+    public @ResponseBody Object getJSONresponse(Model model) {
+        return accountsBean;
+    }
+
+    @RequestMapping("/json")
+    public @ResponseBody String getJson(WebRequest request, Model model) throws JsonProcessingException {
+        request.getLocale();
+        ObjectMapper mapper = new ObjectMapper();
+        return mapper.writeValueAsString(context.getBean("accountsBean")) + "<br> " + request.getLocale();
     }
 
 }
--- weblayer/src/main/webapp/WEB-INF/testpage.jsp
+++ weblayer/src/main/webapp/WEB-INF/testpage.jsp
@@ -1,4 +1,5 @@
 <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@ taglib prefix="form" uri="http://www.springframework.org/tags/form" %>
 <%@ page import="java.util.Enumeration" %>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html xmlns="http://www.w3.org/1999/xhtml"
@@ -7,24 +8,27 @@
 <head>
     <title></title>
 </head>
-Атрибуты HTTPServletRequest:<br>
-<%
-    String attr;
-    for (Enumeration e = request.getAttributeNames(); e.hasMoreElements(); ) {
-        attr = (String)(e.nextElement());
-        %>
-        <%= attr%> - <%= request.getAttribute(attr).toString()%>
-        <br>
-        <%
-    }
-%>
 
-<c:forEach var="counter" begin="1" end="${requestScope.acc.getAccounts().size()}">
-    <jsp:text>${requestScope.acc.getAccounts().get(counter-1)}</jsp:text>
+
+<body>
+<form:form action="<c:url value='j_spring_security_exit_user'/>"  method="POST">
+    <input  name="Logout" type="submit" value="log out"/>
+</form:form>
+
+Typical JavaBean: <br>
+<c:forEach var="val" items="${requestScope.acc.getAccounts()}">
+    <jsp:text>${val} </jsp:text>
+    <br>
+</c:forEach>
+
+RequestScope: <br>
+<c:forEach var="val" items="${requestScope}">
+    <jsp:text>${val.key} - ${val.value}</jsp:text>
     <br>
 </c:forEach>
 
 
-<body>
+
+
 </body>
 </html>
