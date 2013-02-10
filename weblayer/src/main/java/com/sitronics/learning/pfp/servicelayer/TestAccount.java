908cbb0d66b7ab8fdaedfaa487124562fe2fd5c8
Capt Smollet <capt.smollet@hotmail.com>
2013-02-11 03:27:40 +0400

    initial

--- weblayer/src/main/java/com/sitronics/learning/pfp/servicelayer/TestAccount.java
+++ weblayer/src/main/java/com/sitronics/learning/pfp/servicelayer/TestAccount.java
@@ -1,16 +1,21 @@
 package com.sitronics.learning.pfp.servicelayer;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.format.annotation.DateTimeFormat;
+
 import java.math.BigDecimal;
 import java.sql.Date;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Random;
 
 /**
  * Date: 06.02.13
  */
 public class TestAccount {
     private int id;
-    private Date date;
+    private java.util.Date utilDate;
+    private java.sql.Date date;
     private BigDecimal amount;
 
     public TestAccount() throws ParseException {
@@ -23,6 +28,16 @@ public class TestAccount {
         this.id = id;
         this.date = new Date(new SimpleDateFormat("dd.MM.yyyy").parse(date).getTime());
         this.amount = amount;
+        Random r = new Random();
+        this.utilDate = new SimpleDateFormat("dd.MM.yyyy").parse((r.nextInt(28) + 1) + "." + (r.nextInt(11) + 1) + ".20" + r.nextInt(13));
+    }
+
+    public java.util.Date getUtilDate() {
+        return utilDate;
+    }
+
+    public void setUtilDate(java.util.Date utilDate) {
+        this.utilDate = utilDate;
     }
 
     public int getId() {
@@ -55,6 +70,7 @@ public class TestAccount {
                 "id=" + id +
                 ", date=" + date +
                 ", amount=" + amount +
+                ", utilDate=" + utilDate +
                 '}';
     }
 }
--- weblayer/src/main/webapp/WEB-INF/jsp/testpage.jsp
+++ weblayer/src/main/webapp/WEB-INF/jsp/testpage.jsp
@@ -1,11 +1,13 @@
 <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <%@ taglib prefix="form" uri="http://www.springframework.org/tags/form" %>
+<%@ taglib prefix="spring" uri="http://www.springframework.org/tags" %>
 <%@ page import="java.util.Enumeration" %>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html xmlns="http://www.w3.org/1999/xhtml"
       xmlns:jsp="http://java.sun.com/JSP/Page"
       xmlns:c="http://java.sun.com/jsp/jstl/core">
 <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <title></title>
 </head>
 
@@ -15,11 +17,19 @@
     <input  name="Logout" type="submit" value="log out"/>
 </form>
 
+<h3>
+    Locale: <spring:message code="error.test_message" text="default text" />
+</h3>
+
+Current Locale : ${pageContext.response.locale}<br>
+
+
 Typical JavaBean: <br>
-<c:forEach var="val" items="${requestScope.acc.getAccounts()}">
+<c:forEach var="val" items="${acc.getAccounts()}">
     <jsp:text>${val} </jsp:text>
     <br>
 </c:forEach>
+Formatted date: <c:out value="${date}"/> <br>
 
 RequestScope: <br>
 <c:forEach var="val" items="${requestScope}">
